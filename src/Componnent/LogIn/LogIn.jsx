1c0b4b0150db25cae7b086acd35bd529c5eed64e
srshakil2 <rh3642921@gmail.com>
2025-05-17 18:56:07 +0600

    added more pacage

--- src/Componnent/LogIn/LogIn.jsx
+++ src/Componnent/LogIn/LogIn.jsx
@@ -1,6 +1,11 @@
+import { Helmet } from "react-helmet";
+
 const LogIn = () => {
   return (
     <div>
+      <Helmet>
+        <title>AaruConnect | Login</title>
+      </Helmet>
       <h3 className="text-5xl font-extrabold text-center text-emerald-600">
         This is Login page
       </h3>
